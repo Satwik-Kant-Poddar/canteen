14fba6b4d2e213a62211fc449f8e1e28165fdb87
Satwik Kant Poddar <satwikkantpoddar@gmail.com>
2026-01-02 12:51:20 +0530

    First commit

---
+++ src/main/java/com/office/canteen/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.office.canteen.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Canteen application is running";
+    }
+}
